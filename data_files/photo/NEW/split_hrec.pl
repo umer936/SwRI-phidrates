fcda99007b91046ae51a7fd7c4d477810fbd6151
joey <joey@swri.edu>
2015-01-27 15:05:17 +0000

    Don't remember

---
+++ data_files/photo/NEW/split_hrec.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl -w
+
+
+#@filenames = ("hrec1.dat", "hrec2.dat", "hrec3.dat", "hrec4.dat", "hrec5.dat");
+#@filenames = ("HRec1Sup.dat");
+@filenames = ("HRec1A.txt", "HRec2A.txt");
+
+foreach $file (@filenames) {
+
+    print "Working with $file\n";
+    open (INPUT_FILE, "< $file") || die ("ERROR - Couldn't open $file!\n");
+
+START_OF:
+
+    $line = <INPUT_FILE>;
+    if (!eof (INPUT_FILE)) {
+        @fields = split (/\s+/, $line);
+        open (OUTPUT_FILE, "> hrecs/$fields[4].dat") || 
+            die ("ERROR - Couldn't open $fields[4].dat\n");
+        print "Writing hrecs/$fields[4].dat\n";
+        print OUTPUT_FILE $line;
+        while ($line = <INPUT_FILE>) {
+            print OUTPUT_FILE $line;
+            if ($line =~ /\*\*\*/) {
+                close (OUTPUT_FILE);
+                goto START_OF;
+            }
+        }
+    }
+    close (INPUT_FILE);
+}
